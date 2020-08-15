743210612e61f803a6d50924d7db90f6262ee06c
rajkumar-101 <rajmummies@gmail.com>
2020-08-15 18:44:48 +0530

    Updates

---
+++ precache-manifest.d472e6df6e0366dfe3010b0f5b674de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c27fd662ab87c33431a3f31e8755515",
+    "url": "/React-todo-list-app/index.html"
+  },
+  {
+    "revision": "cb86aed1aef2ac6b956d",
+    "url": "/React-todo-list-app/static/css/main.5e03366b.chunk.css"
+  },
+  {
+    "revision": "2d963f83f81af84e5576",
+    "url": "/React-todo-list-app/static/js/2.4be7c67a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-todo-list-app/static/js/2.4be7c67a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb86aed1aef2ac6b956d",
+    "url": "/React-todo-list-app/static/js/main.039424bf.chunk.js"
+  },
+  {
+    "revision": "1c32e22ba41048470cdc",
+    "url": "/React-todo-list-app/static/js/runtime-main.d8d9a226.js"
+  }
+]);
